7262454beb0d54984d15fa357ee1b8007568539a
Matheus Chiarello <chiarellomatheus@gmail.com>
2023-09-05 19:47:40 -0300

    desenvolvendo o projeto de acordo com o informado no dia 01

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
-import './style';
+import validator from 'validator';
+import './style.css';
 
-console.log('Hello World!');
+const warning = document.querySelector('#warning');
+const button = document.querySelector('#button');
+const input = document.querySelector('#input');
+const select = document.querySelector('#select');
+
+button.addEventListener('click', () => {
+  switch (select.value) {
+  case 'Email': {
+    const testEmail = validator.isEmail(input.value);
+    warning.innerHTML = `A validação retornou ${testEmail}`;
+    break;
+  }
+  case 'CPF': {
+    const testCPF = validator.isInt(input.value)
+      && validator.isLength(input.value, { min: 11, max: 11 });
+    warning.innerHTML = `A validação retornou ${testCPF}`;
+    break;
+  }
+  default: {
+    console.log('Deu ruim!');
+    break;
+  }
+  }
+});
